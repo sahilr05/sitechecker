efbdd6bef7fd13c091f2cfb7746817aece2d0a7e
sahilr05 <sahilrajpal85@gmail.com>
2020-06-05 22:17:09 +0530

    Use class based views login/logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from accounts import views as accounts_views
@@ -5,8 +6,12 @@ from accounts import views as accounts_views
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", accounts_views.login_request, name="login"),
-    path("logout", accounts_views.logout_request, name="logout"),
+    # path("login/", accounts_views.login_request, name="login"),
+    # path("logout", accounts_views.logout_request, name="logout"),
+    path(
+        "login", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
     path("delete_user/<int:pk>", accounts_views.delete_user, name="delete_user"),
     path("users", accounts_views.user_list, name="user_list"),
     path("adduser", accounts_views.add_user, name="add_user"),
